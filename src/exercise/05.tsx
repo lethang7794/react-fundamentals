65d6d5b340a489e0896e69fbb12de8a95015e0e7
Thang Le Quang <lethang7794@gmail.com>
2022-03-06 14:45:42 +0700

    Lesson 5, 7

--- src/exercise/05.tsx
+++ src/exercise/05.tsx
@@ -1,22 +1,24 @@
 // TypeScript with React
 // http://localhost:3000/isolated/exercise/05.tsx
+// https://www.typescriptlang.org/play?#code/JYWwDg9gTgLgBAJQKYEMDG8BmUIjgcilQ3wCgB6cuAFQE8wkBlNKYMeAd2BgAtFiYFKjxgwwALkoAbCGhRSeEAM4xxAZgAMW8sCUQpKGEgAm5JAA8kUNLqTkNAVgB0MJedKkY9JHADyDKENoADEAOzgAXjgACikkTFU4UIBXEAAjKwAaOFYAcxFxJNSMqABKSIA+IvSrUjQIUJU4FlQjfytDYAalSLgAHl84CyNQ4x7keqhjPpVWUNzs9sCYENCKiuiIAM7uwt9yiKqtjpguxo96xvhj5bOeqJbDJCWdxuiAb1I4AgBqfELYvEYNk8iIDlU4gk4D8csB8sCvgQALT-GKQ4Gw+HguDouBIzEiTKI-AAKlRgISILhYMqOKBcBJBIR33w5HJ6KpWNpuKooIRAF9Sh4vAw4ABheRoZIGFZQAAKODA9zgn2+6IA-IUUjUoIibkEoJq4ABrJC0CCYOAipAWuD607dRF8o3akqkfmkTDJUIYM7iyXSg0fOlQqIabL26C9fB-TkiXoaOD8woSqRSmXQBVbJTlVXNbrwIhKaXwKL2u4AbUjUAAuhSMXyhd8iDBklBwtFEd8+sZgAA3Cpd779erGJCD4eTlXo-kq6uz9582dRPoQZJidcVRdIYtSGD8vrkNcbmATqeH0fjoeH3sDxFCj2e72+hpwACCYDA0VzTqQrfbMTXreZ6Tn0PAAIwVKm6YGoeEEgcOfTQYGsohjAETvOBs7VhEABEPy4UyGEAEyzuQCHdshGZQGhGFYXa2yynhSKEXyJFkRR-RUQatGYdhjHQHhJKsdS6HvKRcDkUOlEBtRvH0ThuHkCJ8LsZJCE3v2Z4Ph4FiQLAKrvp+SakEAA
 
-// 🦺 add type definitions for each function
-const operations = {
-  '+': (left: number, right: number): number => left + right,
-  '-': (left: number, right: number): number => left - right,
-  '*': (left: number, right: number): number => left * right,
-  '/': (left: number, right: number): number => left / right,
+type Operator = '+' | '-' | '*' | '/'
+type OperatorFn = (left: number, right: number) => number
+type Operations = Record<Operator, OperatorFn>
+
+const operations: Operations = {
+  '+': (left, right) => left + right,
+  '-': (left, right) => left - right,
+  '*': (left, right) => left * right,
+  '/': (left, right) => left / right,
 }
 
-// 🦺 create a type called CalculatorProps
 type CalculatorProps = {
-  left: number
-  operator: string
-  right: number
+  left?: number
+  operator?: Operator
+  right?: number
 }
-// 🦺 set the type for this props argument to CalculatorProps
-function Calculator({left, operator, right}: CalculatorProps) {
+function Calculator({left = 0, operator = '+', right = 0}: CalculatorProps) {
   const result = operations[operator](left, right)
   return (
     <div>
